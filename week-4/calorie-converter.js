c75c050cae6ffd50ebd27268daa3ed70e8a91c02
Chris Gorham <chris.gorham@outlook.com>
2023-01-25 15:56:54 -0500

    Update calorie-converter.js

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -13,7 +13,7 @@
 ;=====================================
 */
 
-import { FoodModel } from "./food-model";
+import { FoodModel } from "./food-model.js";
 
 export class CalorieConverter {
     static data = [
