cf9b8b8f51650373a55a8ea81ac857dee7fa0b0f
Chris Gorham <chris.gorham@outlook.com>
2023-01-24 22:59:27 -0500

    initial files (not complete)

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,10 @@
+/*
+======================================
+; Title: calorie-converter.js 
+; Author: Professor Krasso
+; Date: 25 Jan 2023
+; Modified By: Chris Gorham
+; Description: This code is used for the calorie converter in the calorie app
+; Sources Used: Web 330 HTML, CSS, and JavaScript Requirements Word Document
+;=====================================
+*/
---
+++ week-4/food-model.js
@@ -0,0 +1,10 @@
+/*
+======================================
+; Title: food-model.js 
+; Author: Professor Krasso
+; Date: 25 Jan 2023
+; Modified By: Chris Gorham
+; Description: This code is used for the food model in the calorie app
+; Sources Used: Web 330 HTML, CSS, and JavaScript Requirements Word Document
+;=====================================
+*/
