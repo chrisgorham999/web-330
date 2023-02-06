f69b873df6edcc0db34ef4710e2ffafbb5e5e1b6
Chris Gorham <chris.gorham@outlook.com>
2023-02-06 12:27:20 -0500

    week 6 initial setup files

---
+++ week-6/float-field.js
@@ -0,0 +1,10 @@
+/*
+======================================
+; Title: float-fields.js 
+; Author: Professor Krasso
+; Date: 06 Feb 2023
+; Modified By: Chris Gorham
+; Description: This code is used for the float fields in the Future Value App assignment
+; Sources Used: Web 330 HTML, CSS, and JavaScript Requirements Word Document
+;=====================================
+*/
