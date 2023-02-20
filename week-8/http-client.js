b1f04201f160571603338825f9af66441f584332
Chris Gorham <chris.gorham@outlook.com>
2023-02-20 07:45:22 -0500

    week 8 initial setup

---
+++ week-8/http-client.js
@@ -0,0 +1,14 @@
+/*
+======================================
+; Title: http-client.js 
+; Author: Professor Krasso
+; Date: 20 Feb 2023
+; Modified By: Chris Gorham
+; Description: This code is used for the http client in the WhatABook, Part 2 App assignment
+; Sources Used: Web 330 HTML, CSS, and JavaScript Requirements Word Document
+;=====================================
+*/
+
+export class HttpClient {
+
+}
